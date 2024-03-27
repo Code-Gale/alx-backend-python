ea5d17735d4942f41a59f391119c0d07b2b08379
Code-Gale <codewhiz001@gmail.com>
2024-03-27 15:26:55 +0100

    make multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,7 +2,10 @@
 ''' type-annotated func for multiplier '''
 
 
-def make_multiplier(multiplier: float) -> callable:
+import typing
+
+
+def make_multiplier(multiplier: float) -> typing.callable:
     '''returns a function that multiplies a float by multiplier'''
     def multiply(n: float) -> float:
         '''returns product of n and multiplier'''
