fe133c8121550a578c304233cca8f945534e061e
Code-Gale <codewhiz001@gmail.com>
2024-03-27 14:47:51 +0100

    comments

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-# type-annotated func for add
+''' type-annotated func for add '''
+
+
 def add(a: float, b: float) -> float:
-    '''
-    returns sum of two floats    
-    '''
+    '''returns sum of two floats'''
     return (a + b)
